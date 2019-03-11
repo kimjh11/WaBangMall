bc56aca074a57116a552a650a84d66bbce8c81ba
JUNHOKIM\JUNHO <kimjunho1130@gmail.com>
2019-03-11 18:03:48 +0900

    준호 CommandQnaView 푸쉬

--- src/kr/wabang/qna/CommandQnaView.java
+++ src/kr/wabang/qna/CommandQnaView.java
@@ -14,17 +14,22 @@ public class CommandQnaView implements CommandService {
 	public String process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		// 질문 보기
 		QnaVO vo = new QnaVO();
+		// 답변 보기
+		AnswerVO answervo = new AnswerVO();
 		
 		//글 번호
 		vo.setQ_num(Integer.parseInt(req.getParameter("q_num")));
+		answervo.setQ_num(Integer.parseInt(req.getParameter("q_num")));
 		
 		//현재 페이지 번호
 		vo.setNum(Integer.parseInt(req.getParameter("num")));
 		
 		QnaDAO dao = new QnaDAO();
 		dao.selectQnaView(vo);
+		dao.selectAnswer(answervo);
 		
 		req.setAttribute("vo", vo);
+		req.setAttribute("answervo", answervo);
 		
 		return "qnaView.jsp";
 	}
